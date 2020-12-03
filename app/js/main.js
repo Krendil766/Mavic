7c292d9eb213b001155dc864255c9ca85acbc33d
Krendil766 <krendil766@gmail.com>
2020-12-04 02:42:41 +0300

    Added four and five page css

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,10 @@ $(function() {
         prevArrow: '<button class = "slider-arrows slider-arrows__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
         nextArrow: '<button class = "slider-arrows slider-arrows__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></button>',
     });
+
+    $('.questions__item-title').on('click', function() {
+        $('.questions__item').removeClass('questions__item-active');
+        $(this).parent().addClass('questions__item-active');
+        console.log('+');
+    })
 });
